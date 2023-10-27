02e79c467a5924414466fcc29a1b2a5a15bcce83
yom1215 <yoko1215@hotmail.com>
2023-10-27 14:17:05 +0900

    Update regression_app.py

--- regression_app.py
+++ regression_app.py
@@ -20,12 +20,14 @@ def perform_regression(train_file, test_file):
 
     # 訓練データに対する予測値の取得
     train_data['prediction'] = model.predict(X_train)
+    train_data['is_test'] = 0
     
     # 予測
     predictions = model.predict(X_test)
     test_data['prediction'] = predictions
+    test_data['is_test'] = 1
     
-    df = pd.concat([test_data,train_data], axis=0)
+    df = pd.concat([train_data,test_data], axis=0)
 
     return df, predictions
 
@@ -34,9 +36,21 @@ def main():
     st.title("回帰モデルアプリ")
 
     # ファイルアップロード
-    st.write("データのアップロード")
+    st.markdown(```
+        ## データ
+        - データに時間軸がある場合、上から「古い→新しい」順にしてください
+        - trainにだけ予測する値を列名「target」でいれてください
+        - testには「target」列はいれないでください
+        - trainとtestのそのほかの列は同じ順番に並べてください
+        - trainの「target」以外の列すべてで学習を行います
+        ## モデル
+        - scikit-learnの線形回帰です
+        ```)
+    
     train_file = st.file_uploader("訓練データをアップロードしてください", type=["csv"])
     test_file = st.file_uploader("テストデータをアップロードしてください", type=["csv"])
+
+    start_button = st.button("学習・推論実行")
     
     # Demoボタンの追加
     st.write("demo: サンプルデータを読み込んで実行")
@@ -47,25 +61,24 @@ def main():
         st.write("サンプルデータを使用してモデルを訓練・実行します...")
         train_file = './train.csv'
         test_file = './test.csv'
+        start_button = True
     
-    if train_file and test_file:
+    if start_button:
         if not demo_button:  # ボタンが押されていない場合のメッセージ
-            st.write("モデルを訓練・実行します...")
-        
-        df, predictions = perform_regression(train_file, test_file)
+            st.write("入力データを使用してモデルを訓練・実行します...")
 
+        with st.spinner(text="In progress..."):
+            df, predictions = perform_regression(train_file, test_file)
+        
+        st.success('Done!')
 
         # 予測結果の表示
         st.write("訓練データの実際のターゲット値と予測値:")
-        plot_len = len(predictions)+10
-        st.line_chart(df[['target','prediction']].iloc[:plot_len])
+        df['is_test'] = df.is_test*max(df[['target','prediction']])*1.2
+        st.line_chart(df[['target','prediction','is_test']],color=['lightblue','red','blue'])
 
-        # 予測結果をDataFrameに変換
-        df_predictions = pd.DataFrame(predictions, columns=["predictions"])
-
-        # 予測結果の表示
-        st.write("予測結果:")
-        st.write(df_predictions)
+        st.write("test 予測結果:")
+        st.write(predictions)
 
         # CSVダウンロードボタンの表示
         csv = df.to_csv(index=False)
